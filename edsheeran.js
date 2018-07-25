5e71a5192cf583f1a3e2852fffb76c11f3451c6f
eyevent <eyevent@USSEAGirls07.ey.net>
2018-07-25 14:01:01 -0700

    first commit

---
+++ edsheeran.js
@@ -0,0 +1,27 @@
+function revealAnswer(){
+  console.log("function working");
+  var answer = "because sheeran away!!!! hahahahaha";
+  var joke = document.getElementById("joke");
+  joke.innerHTML = answer;
+}
+
+function revealAnswer2(){
+  console.log("function working");
+  var answer = "a SHEERan!!!! hahahahaha";
+  var joke = document.getElementById("joke2");
+  joke.innerHTML = answer;
+}
+
+function revealAnswer3(){
+  console.log("function working");
+  var answer = "ginger snaps!!!! hahahahaha";
+  var joke = document.getElementById("joke3");
+  joke.innerHTML = answer;
+}
+
+function revealAnswer4(){
+  console.log("function working");
+  var answer = "because he only knows the shape of you!!!! hahahahaha";
+  var joke = document.getElementById("joke4");
+  joke.innerHTML = answer;
+}
